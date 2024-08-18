521b02aa48845459fbe8eb2d1017e2454cd10396
Gokulasigamani <113229521+Gokulasigamani@users.noreply.github.com>
2024-08-19 00:44:50 +0530

    email functinality

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("listening..");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
+
+const upload = multer({ storage: storage });
+
 
 app.post("/login", (req, res) => {
   const users = [
@@ -18,9 +29,10 @@ app.post("/login", (req, res) => {
     { rollnumber: "727621bit053", password: "06042004" },
     { rollnumber: "727621bec301", password: "25102003" },
     { rollnumber: "727621bec013", password: "23052004" },
+    { rollnumber: "727621bit031", password: "23052004" },
   ];
 
-  const {rollnumber ,password}=req.body;
+  const { rollnumber, password } = req.body;
 
   const user = users.find(
     (user) => user.rollnumber === rollnumber && user.password === password
@@ -31,4 +43,17 @@ app.post("/login", (req, res) => {
   } else {
     res.status(400).json({ message: "Invalid roll number or password" });
   }
+});
+
+app.post("/complaints", upload.single("attachment"), (req, res) => {
+  const { complaint } = req.body;
+  const attachment = req.file ? req.file.path : null;
+
+  console.log("Complaint:", complaint);
+  console.log("File:", attachment);
+  res.status(200).json({ message: "Complaint received successfully" });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000..");
 });
